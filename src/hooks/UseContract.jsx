3b139d9a0a118048fc326572e4e930a237dd304f
user <user@XVIII-DEMS.local>
2023-06-28 07:06:44 +0100

    fixed doctorsrecord bug

--- src/hooks/UseContract.jsx
+++ src/hooks/UseContract.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 import abi from "../abi.json";
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
-
-export const contracts = ()=>{
+export const contracts = () => {
   const contractAddress = "0xB8f1ed9Adca8c6863B3da364B1b332B51462BA06";
-    if (window.ethereum && window.ethereum.isMetaMask) {
-        const tempProvider = new ethers.providers.Web3Provider(window.ethereum);
-        // setProvider(tempProvider);
-  
-        const tempSigner = tempProvider.getSigner();
-        // setSigner(tempSigner);
-        
-        const tempContract = new ethers.Contract(contractAddress, abi, tempSigner);
-      return {tempContract}
-    }
-    
-}
+  if (window.ethereum && window.ethereum.isMetaMask) {
+    const tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+    // setProvider(tempProvider);
 
+    const tempSigner = tempProvider.getSigner();
+    // setSigner(tempSigner);
+
+    const tempContract = new ethers.Contract(contractAddress, abi, tempSigner);
+    return tempContract;
+  }
+};
